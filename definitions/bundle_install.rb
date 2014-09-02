90ff52ce37dcb2748f79bbeff95ca1a49acba51c
aadamson <aadamson@stanford.edu>
2014-09-02 13:17:06 -0700

    using orrect bundle

--- definitions/bundle_install.rb
+++ definitions/bundle_install.rb
@@ -7,7 +7,7 @@ define :bundle_install do
   # bundle install
   execute 'bundle install' do
     cwd params[:path]
-    command "bundle install"
+    command "/usr/local/bin/bundle install"
     user params[:user]
     only_if { File.directory?(params[:path]) }
   end
